59978c354b941df78c4718b12f151b0af81f2359
Thet Paing Lin <thatpainlin1999@gmail.com>
2024-12-21 15:14:52 +0000

    Add base.html

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configuare application
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("base.html")
